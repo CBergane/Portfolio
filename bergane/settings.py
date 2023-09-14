b42411b05976ba24f7369aa050c5ce8597694500
CBergane <christian.bergane@gmail.com>
2023-09-14 10:58:10 +0000

    added to requirements

--- bergane/settings.py
+++ bergane/settings.py
@@ -183,7 +183,7 @@ CSRF_TRUSTED_ORIGINS = [
 
 EMAIL_BACKEND = os.environ.get("EMAIL_BACKEND")
 EMAIL_HOST = os.environ.get("EMAIL_HOST")
-EMAIL_PORT = 465  # usually this is the port number for TLS
+EMAIL_PORT = 587  # usually this is the port number for TLS
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
