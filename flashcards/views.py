320918ba9022085672e3d59271912526f7bddf92
CBergane <christian.bergane@gmail.com>
2023-12-05 23:03:05 +0000

    fixed a smal bug

--- flashcards/views.py
+++ flashcards/views.py
@@ -7,10 +7,11 @@ def category_list(request):
     return render(request, 'flashcards/category_list.html', {'categories': categories})
 
 def flashcard_list(request, category_id):
+    category = Category.objects.get(id=category_id)
     flashcards = Flashcard.objects.filter(category_id=category_id).prefetch_related('choices')
     paginator = Paginator(flashcards, 10)  # Visa 10 kort per sida
 
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
-    return render(request, 'flashcards/flashcard_list.html', {'page_obj': page_obj})
+    return render(request, 'flashcards/flashcard_list.html', {'page_obj': page_obj, 'category_name': category.name})
 
