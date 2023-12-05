e1a4e8c3958b80b1acaa48a269748496604fc7c3
CBergane <christian.bergane@gmail.com>
2023-12-05 21:39:19 +0000

    added reset button to cards and some style

--- flashcards/views.py
+++ flashcards/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
 from .models import Category, Flashcard
 
 def category_list(request):
@@ -6,6 +7,10 @@ def category_list(request):
     return render(request, 'flashcards/category_list.html', {'categories': categories})
 
 def flashcard_list(request, category_id):
-    flashcards = Flashcard.objects.filter(category_id=category_id).prefetch_related('choices')
-    return render(request, 'flashcards/flashcard_list.html', {'flashcards': flashcards})
+    flashcards = Flashcard.objects.filter(category_id=category_id)
+    paginator = Paginator(flashcards, 10)  # Visa 10 kort per sida
+
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return render(request, 'flashcards/flashcard_list.html', {'page_obj': page_obj})
 
