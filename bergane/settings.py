faad362519082ab97822090aaa48c10092b440a7
CBergane <christian.bergane@gmail.com>
2023-12-07 07:42:20 +0000

    removed debug

--- bergane/settings.py
+++ bergane/settings.py
@@ -33,7 +33,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['www.cbergane.se', 'cbergane.se','cbergane.herokuapp.com', 'localhost', '127.0.0.1', '8000-cbergane-portfolio-sf39lqfknqa.ws-eu106.gitpod.io']
 
