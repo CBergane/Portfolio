68310569a131d1eb47454af031acbd2f7975e296
CBergane <christian.bergane@gmail.com>
2023-12-11 09:32:58 +0000

    order questions with ID

--- flashcards/views.py
+++ flashcards/views.py
@@ -9,7 +9,7 @@ def category_list(request):
 
 def flashcard_list(request, category_id):
     category = Category.objects.get(id=category_id)
-    flashcards = Flashcard.objects.filter(category_id=category_id).prefetch_related('choices')
+    flashcards = Flashcard.objects.filter(category_id=category_id).prefetch_related('choices').order_by('id')
     paginator = Paginator(flashcards, 10)  # Visa 10 kort per sida
 
     page_number = request.GET.get('page')
