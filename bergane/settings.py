42c8fb74fb506c013cba0cc9ee26cc2834aec18c
CBergane <christian.bergane@gmail.com>
2024-03-11 12:22:41 +0000

    added new database

--- bergane/settings.py
+++ bergane/settings.py
@@ -31,7 +31,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['www.cbergane.se', 'cbergane.se','cbergane.herokuapp.com', 'localhost', '127.0.0.1', '8000-cbergane-portfolio-b1jl0xvmjmj.ws-eu108.gitpod.io']
 
