09f710d71e7a5beaec4494af360cbc87e9880623
CBergane <christian.bergane@gmail.com>
2024-03-31 14:46:53 +0000

    added styleing

--- bergane/settings.py
+++ bergane/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['www.cbergane.se', 'cbergane.se','cbergane.herokuapp.com', 'localhost', '127.0.0.1', '8000-cbergane-portfolio-b1jl0xvmjmj.ws-eu108.gitpod.io']
+ALLOWED_HOSTS = ['www.cbergane.se', 'cbergane.se','cbergane.herokuapp.com', 'localhost', '127.0.0.1', '8000-cbergane-portfolio-b1jl0xvmjmj.ws-eu110.gitpod.io']
 
 SITE_ID = 1
 
